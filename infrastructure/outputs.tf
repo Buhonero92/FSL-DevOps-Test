083739c3e343f57a4700439d07ec64f2ea361ba1
Carlos Eduardo Zuleta Uribe <czuletauribe@rccl.com>
2024-11-01 10:38:13 -0500

    feature/fixing the if conditions

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -3,5 +3,5 @@ output "bucket_name" {
 }
 
 output "website_endpoint" {
-  value = aws_s3_bucket.static_webapp_bucket.website_endpoint
+  value = "http://${aws_s3_bucket.static_webapp_bucket.bucket}.s3-website-us-east-1.amazonaws.com "
 }
