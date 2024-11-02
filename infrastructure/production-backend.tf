c3d8a520f7b3a50efdba785595e70c72e79db5e3
Carlos Eduardo Zuleta Uribe <czuletauribe@rccl.com>
2024-11-02 10:01:07 -0500

    feature/adding terraform state backend remotely

--- infrastructure/production-backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-production-636655795335"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock-table-production"
-    encrypt        = true
-  }
-}
